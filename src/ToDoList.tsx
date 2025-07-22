5b7797f54fb47641f547903b100e837449720d21
Alex-CJ <petruciliuta1@gmail.com>
2025-07-22 13:56:36 +0300

    Formated code and added delete all button

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,88 +1,149 @@
-import {useState} from 'react';
+import { useState } from "react";
 
 export default function ToDoList() {
-    const [tasks, setTasks] = useState<string[]>(['Eat breakfast', 'Go shower', 'Walk the dog'])
-    const [newTask, setNewTask] = useState<string>('');
+  const [tasks, setTasks] = useState<string[]>([
+    "Eat breakfast",
+    "Go shower",
+    "Walk the dog",
+  ]);
+  const [newTask, setNewTask] = useState<string>("");
 
-    function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
-        setNewTask(event.target.value);
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setNewTask(event.target.value);
+  }
+
+  function addTask() {
+    if (newTask.trim() !== "") {
+      setTasks((t) => [newTask, ...t]);
+      setNewTask("");
     }
-    
-    function addTask() {
+  }
 
-        if(newTask.trim() !== '') {
-            setTasks(t=>[...t, newTask]);
-            setNewTask('');
-        }
+  function deleteAllTasks() {
+    setTasks([]);
+  }
+
+  function deleteTask(index: number) {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  }
+
+  function moveTaskUp(index: number) {
+    if (index > 0) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index - 1]] = [
+        updatedTasks[index - 1],
+        updatedTasks[index],
+      ];
+      setTasks(updatedTasks);
     }
+  }
 
-    function deleteTask(index: number) {
-        const updatedTasks = tasks.filter((_, i)=> i !== index);
-        setTasks(updatedTasks);
+  function moveTaskDown(index: number) {
+    if (index < tasks.length - 1) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index + 1]] = [
+        updatedTasks[index + 1],
+        updatedTasks[index],
+      ];
+      setTasks(updatedTasks);
     }
+  }
 
-    function moveTaskUp(index: number) {
-        if(index>0){
-            const updatedTasks = [...tasks];
-            [updatedTasks[index],updatedTasks[index-1]]=
-            [updatedTasks[index-1],updatedTasks[index]];
-            setTasks(updatedTasks);
-        }
-    }
+  return (
+    <div className="to-do-list">
+      <h1>To-Do-List</h1>
 
-    function moveTaskDown(index: number) {
-        if(index<tasks.length -1){
-            const updatedTasks = [...tasks];
-            [updatedTasks[index],updatedTasks[index+1]]=
-            [updatedTasks[index+1],updatedTasks[index]];
-            setTasks(updatedTasks);
-        }
-    }
-
-    return(<div className="to-do-list">
-
-        <h1>To-Do-List</h1>
-
-        <div>
-            <input
-                type='text'
-                placeholder='Enter a task...'
-                value={newTask}
-                onChange={handleInputChange}
-                onKeyDown={(e) => {
-                    if (e.key === 'Enter') addTask();
-                }}
-                />
-            <button 
-            className='add-button'
-                onClick={addTask}>
-                Add
-            </button>
-        </div>
-
+      <div>
+        <input
+          type="text"
+          placeholder="Enter a task..."
+          value={newTask}
+          onChange={handleInputChange}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") addTask();
+          }}
+        />
+        <button
+          className="add-button"
+          onClick={addTask}
+          disabled={!newTask.trim()}
+        >
+          Add
+        </button>
+        <button className="delete-all-button" onClick={deleteAllTasks} disabled={tasks.length === 0}>
+          Delete All
+        </button>
+      </div>
+      {tasks.length > 0 ? (
         <ol>
-            {tasks.map((task, index)=>
-                <li key={index}>
-                    <span className='text'>{task}</span>
-                    <button
-                        className='delete-button'
-                        onClick={()=> deleteTask(index)}>
-                            Delete
-                    </button>
-                    <button
-                        className='move-button'
-                        onClick={()=> moveTaskUp(index)}>
-                            ☝
-                    </button>
-                    <button
-                        className='move-button'
-                        onClick={()=> moveTaskDown(index)}>
-                            👇
-                    </button>
-                </li>
-            )}
+          {tasks.map((task, index) => (
+            <ToDoListItem
+              key={index}
+              task={task}
+              index={index}
+              onDelete={deleteTask}
+              onMoveDown={moveTaskDown}
+              onMoveUp={moveTaskUp}
+              isFirst={index === 0}
+              isLast={index === tasks.length - 1}
+            />
+          ))}
         </ol>
+      ) : (
+        <EmptyState />
+      )}
+    </div>
+  );
+}
 
+interface ToDoListItemProps {
+  task: string;
+  index: number;
+  isFirst?: boolean;
+  isLast?: boolean;
+  onDelete: (index: number) => void;
+  onMoveUp: (index: number) => void;
+  onMoveDown: (index: number) => void;
+}
 
-    </div>);
+function ToDoListItem({
+  task,
+  index,
+  onDelete,
+  onMoveUp,
+  onMoveDown,
+  isFirst,
+  isLast,
+}: ToDoListItemProps) {
+  return (
+    <li key={index}>
+      <span className="text">{task}</span>
+      <button className="delete-button" onClick={() => onDelete(index)}>
+        Delete
+      </button>
+      {isFirst && isLast ? null : (
+        <>
+          <button
+            className="move-button"
+            onClick={() => onMoveUp(index)}
+            disabled={isFirst}
+          >
+            ☝
+          </button>
+          <button
+            className="move-button"
+            onClick={() => onMoveDown(index)}
+            disabled={isLast}
+          >
+            👇
+          </button>
+        </>
+      )}
+    </li>
+  );
+}
+
+function EmptyState() {
+  return <h1>Add your first task</h1>;
 }
